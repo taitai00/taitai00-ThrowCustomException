b8b02a50e9bbf9ae58a508a5443c9be4401a58bb
Khup Taithul <aatuang@gmail.com>
2023-02-16 17:03:44 +0000

    This method throw a CustomException.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
         Lab thrower = new Lab();
         System.out.println("The lab code should throw a CustomException, causing a stack trace to be printed to console here:");
-        thrower.throwCustomException();
+        //thrower.throwCustomException();
         System.out.println("\nIf this line was printed to console, then throwing an exception was unsuccessful.");
     }
 }
